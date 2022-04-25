23ac323088e5861008d1ce2b2511c9e9a6e518e4
Bhargav Patel <bhargavchaniyara342@gmail.com>
2022-04-25 23:11:54 +0530

    added orders

---
+++ resources/views/admin/orders/index.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.master')
+@section('title')
+    {{ $moduleName ?? '' }}
+@endsection
+
+@section('content')
+
+@endsection
+@section('script')
+
+<script>
+     
+</script>
+
+@endsection
