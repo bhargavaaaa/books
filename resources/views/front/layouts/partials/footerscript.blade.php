95f295e67b44b011acffd0d684b9b82c6bd723dd
chirag <viramgamichiragkumar@gmail.com>
2022-04-29 00:34:47 +0530

    front theme integrate

---
+++ resources/views/front/layouts/partials/footerscript.blade.php
@@ -0,0 +1,4 @@
+    <!-- Use Minified Plugins Version For Fast Page Load -->
+    <script src="{{ asset('public/front/js/plugins.js')}}"></script>
+    <script src="{{ asset('public/front/js/ajax-mail.js')}}"></script>
+    <script src="{{ asset('public/front/js/custom.js')}}"></script>
